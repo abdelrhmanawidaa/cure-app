73bf9a803646777ea3f88dcbbcf70ead69db253a
abdelrhman adel <abdelrhmanawidaa@gmail.com>
2023-06-30 08:32:18 +0300

    fixed:gitlab-ci pipeline, added:datadog monitoring tool

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,23 +4,24 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
-  }
-}
-
-provider "aws" {
-  region  = "us-east-1"
-  profile = "vscode"
-
-}
-
-terraform {
-  required_providers {
     datadog = {
       source = "datadog/datadog"
     }
   }
 }
 
+provider "aws" {
+  region  = "us-east-1"
+  profile = "vscode"
+}
+
 provider "datadog" {
   api_key = var.datadog_api_key
 }
+
+variable "datadog_api_key" {
+  description = "Datadog API key"
+  type        = string
+}
+
+# Your resources and configurations go here
